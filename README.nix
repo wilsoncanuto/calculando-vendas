d73e89f37c8027ec7e3aae9796bd9940df1ba2b4
Wilson Canuto Programador Front-End <99556299+wilsoncanuto@users.noreply.github.com>
2023-02-19 17:57:57 -0300

    vendas
    calculando as vendas

---
+++ README.nix
@@ -0,0 +1,9 @@
+# Wilson canuto graciano (2023)!!!!!
+
+# calculando as venadas em python
+
+# vendas = 2100
+
+# custo = 400
+
+# lucro = vendas - custo
---
+++ calculando-vendas.py
@@ -0,0 +1,11 @@
+vendas = 2100
+
+custo = 400
+
+lucro = vendas - custo
+
+print ("O lucro foi de: " "R$" ,lucro, "Reais")
+print ("As vendas foram de: " "R$" ,vendas, "Reais")
+print ("O custo foi de: " "R$" ,custo, "Reais")
+
+ 
